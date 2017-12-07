c2e7d2965e786304b1627a96d21da29388f0153c
Matthis974Jump <matthis974jump@iMac-de-sabrina.local>
2017-12-07 23:04:40 +0400

    #4 Add an event to the bot
    Signed-off-by: Matthis974Jump <matthis974jump@iMac-de-sabrina.local>

--- Bots/Surv/src/main/java/fr/matthis974jump/bots/surv/Main.java
+++ Bots/Surv/src/main/java/fr/matthis974jump/bots/surv/Main.java
@@ -4,15 +4,20 @@ import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.entities.ChannelType;
+import net.dv8tion.jda.core.events.Event;
 import net.dv8tion.jda.core.events.ReadyEvent;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.EventListener;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
-public class Main extends ListenerAdapter implements Runnable {
+import java.util.logging.Logger;
+
+public class Main implements Runnable , EventListener{
     private final String token;
     private final String channel;
     private final String path;
-
+    private JDA jda;
+    private static Logger  l = Logger.getLogger("MainBot");
     public Main(String token, String channel, String pathtomusic) throws Exception {
         this.token = token;
         this.channel = channel;
@@ -22,14 +27,14 @@ public class Main extends ListenerAdapter implements Runnable {
 
     public static void main(String[] args){
         try {
-            new Thread(new Main("","",""),"Main").start();
+            new Thread(new Main("MzgwMjUwNzIzNDE1NDkwNTYz.DQsgWQ.pMxSgywLn2qZgY4mFyIzt2G2aQs","",""),"Main").start();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
     private void connect() throws Exception{
         // Note: It is important to register your ReadyListener before building
-        JDA jda = new JDABuilder(AccountType.BOT)
+        jda = new JDABuilder(AccountType.BOT)
                 .setToken(token)
                 .addEventListener(this)
                 .buildBlocking();
@@ -38,9 +43,7 @@ public class Main extends ListenerAdapter implements Runnable {
 
     }
 
-    @Override
-    public void onMessageReceived(ReadyEvent e)
-    {
-        System.out.println("Ready");
+    public void onEvent(Event event) {
+        if(event instanceof ReadyEvent) l.info("The System is Ready ! :)");
     }
 }
